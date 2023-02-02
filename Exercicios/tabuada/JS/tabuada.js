8c45421ae16d33632f32b07f3a9fc92440200983
Lucas Borguezam <85205753+luscaborguezam@users.noreply.github.com>
2023-02-01 22:46:18 -0300

    01/02/2023 - FInalizando Tabuada

--- Exercicios/tabuada/JS/tabuada.js
+++ Exercicios/tabuada/JS/tabuada.js
@@ -4,15 +4,14 @@ function gerar_tabuada(){
         alert('Preenha o campo "Número"')
     }else{
         numero = parseFloat($numero.value)
-        $select = document.querySelector('div#lista')
+        $select = document.querySelector('select#lista')
         $select.innerHTML = ''
         console.log($select)
         for(let i = 0; i<11; i++){
             let option = document.createElement("option")
             console.log(option)
-            option.setAttribute('id', `option${i}`)
+            option.setAttribute('value', `option${i}`) //item.value =`option${i}`
             option.text = `${numero}X${i}=${numero*i}`
-            // option = document.createTextNode(`${numero}X${i}=${numero*i}`)
             $select.appendChild(option)
         }
     }//fim else
